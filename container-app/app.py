de0fe36eb9eab630cb6bd039330b06bf3313e85b
Rohit Menon <rohit.menon@zivver.com>
2022-01-27 07:01:42 +0100

    Add api endpoint for factorial calculation.

--- container-app/app.py
+++ container-app/app.py
@@ -9,3 +9,11 @@ app = Flask(__name__)
 def default_route():
     return 'Welcome Factorial App'
 
+@app.route('/fact/<int:factNumber>')
+def calculateFact(factNumber):
+    result = '{"Result",'+ str(math.factorial(factNumber))
+    response = app.response_class(
+        response=json.dumps(result),
+        status=200,
+        mimetype='application/json'
+    )
